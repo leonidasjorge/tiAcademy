018bf3fbdfc78a5dec06136e7da8730e0d982844
Leônidas Jorge <leonidasjorge@hotmail.com>
2021-10-20 15:41:10 -0300

    Desafio "Nodejs".
    Estou enviando o desafio do "Ciclo 3", "Node.js".

---
+++ nodejs/desafio/dsfciclo3/models/itempedido.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ItemPedido extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+
+      // ItemPedido "PERTENCE À APENAS UM" Pedido;
+      ItemPedido.belongsTo(models.Pedido, {foreignKey: 'PedidoId', as: 'pedidos'});
+
+      // ItemPedido "PERTENCE À APENAS UM" Servico;
+      ItemPedido.belongsTo(models.Servico, {foreignKey: 'ServicoId', as: 'servicos'});
+    }
+  };
+  ItemPedido.init({
+    quantidade: DataTypes.INTEGER,
+    valor: DataTypes.FLOAT,
+    PedidoId: DataTypes.INTEGER,
+    ServicoId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ItemPedido',
+  });
+  return ItemPedido;
+};
